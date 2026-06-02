178bea52855074634933521f728f462e215334be
Moiz Haider <moizhaider70@gmail.com>
2026-06-02 17:29:18 +0500

    chore(db): db configuration and user entity creation

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './db/data-source';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
